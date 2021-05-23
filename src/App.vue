16560a0f8272efe6ee4a74d7ec95e8ff73e2168b
JoelRubio99 <joel8867@hotmail.com>
2021-05-22 23:20:21 -0500

    Version 1.1

--- src/App.vue
+++ src/App.vue
@@ -28,11 +28,13 @@
 
 								</v-toolbar>
 
-								<form>
+								<form @submit.prevent="submit">
 
 									<v-card flat>
 										<v-container fluid>
 											<v-file-input
+												@change="onFileChange"
+												v-model="file"
 												:rules="rules"
 												counter
 												show-size
@@ -42,6 +44,8 @@
 												label="Rostro">
 											</v-file-input>
 										</v-container>
+
+										<img :src="image" max-width="250" max-height="120">
 									</v-card>
 
 									<v-card flat>
@@ -55,7 +59,7 @@
 												
 												<v-layout row wrap>
 													<v-flex v-for="gender in genders" :key="gender" xs4>
-														<v-checkbox light :label=gender v-model="checkbox">
+														<v-checkbox light :label=gender :value=gender v-model="checkboxes">
 														</v-checkbox>
 													</v-flex>
 												</v-layout>						
@@ -74,14 +78,17 @@
 												elevation="4"
 												color="#3E68D6"
 												class="ma-2 white--text"
-												type="submit">							
+												type="submit"
+												:disabled="!isValid"
+												>							
 												Realizar búsqueda											
 											</v-btn>
 
 											<v-btn
 												rounded												
 												elevation="4"
-												class="ma-2"					
+												class="ma-2"
+												@click="clear()"				
 												>
 												Limpiar búsqueda
 											</v-btn>
@@ -133,7 +140,7 @@
 
 										<v-list-item-content>
 												<iframe src="https://open.spotify.com/embed/album/7FTxFwUINuVrG6ntIaR6Qq?highlight=spotify:track:6DonFmvUg9f2aPWIx39JIL" 
-													width="300" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+													width="500" height="200" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 										</v-list-item-content>
 
 									
@@ -166,7 +173,6 @@
 				<v-col
 					class="text-center"
 					cols="12">
-
 					{{ new Date().getFullYear() }} — <strong>Proyecto de inteligencia artificial</strong>
 				</v-col>
 			</v-footer>
@@ -179,6 +185,8 @@
 
 <script>
 
+//import axios from 'axios';
+
 export default {
 
 	name: 'app',
@@ -186,6 +194,10 @@ export default {
 
 		return {
 
+			file: null,
+			checkboxes: [],
+			isValid: false,
+			image: '',
 			rules: [
 
 				value => !value || value.size < 5000000 || "La imagen no puede pesar más de 5MB"
@@ -207,15 +219,7 @@ export default {
 					active: true
 				},
 				{
-					id: 1,
-					name: 'CFCF',
-					song: 'Test',
-					link: 'https://open.spotify.com/album/7FTxFwUINuVrG6ntIaR6Qq?highlight=spotify:track:6DonFmvUg9f2aPWIx39JIL',
-					img: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
-					active: true
-				},
-				{
-					id: 1,
+					id: 2,
 					name: 'CFCF',
 					song: 'Test',
 					link: 'https://open.spotify.com/album/7FTxFwUINuVrG6ntIaR6Qq?highlight=spotify:track:6DonFmvUg9f2aPWIx39JIL',
@@ -224,8 +228,157 @@ export default {
 				}
 			]
 		}
+	},
+	methods: {
+
+		onFileChange(event) {
+
+			let files = event.target.files || event.dataTransfer.result;
+
+			if (!files.length) 
+				return;
+
+			this.createImage(files[0]);
+		},
+		createImage(file) {
+
+			let reader = new FileReader();
+
+			reader.onload = (event) => {
+
+				this.image = event.target.result;
+			};
+
+			reader.readAsDataURL(file);
+		},
+		clear() {
+
+			this.checkboxes = [];
+
+			this.file = null;
+		},
+		submit() {
+
+			//let data = {imgFile: this.img, genders: this.gender};
+		
+			//validation for data form.
+
+			//doAPICalls(data);			
+		}/*,
+		async doAPICalls(data) {
+
+			try {
+
+				let responseAzure = await requestAPIAzure(data.imgFile);
+
+			} catch (error) {
+
+				console.log(error);
+			}
+
+			let dataRecommendation = getDataRecommendation(responseAzure, data.genders);
+
+			try {
+
+				let responseSpotify = await requestAPISpotify(dataRecommendation);
+
+			} catch (error) {
+
+				console.log(error);
+			}
+			
+		},
+		getDataRecommendation(responseAzure, genders) {
+
+			let emotionsAvailable = responseAzure.emotion.map(emotion => emotion > 0.0);
+
+			/*
+			"emotion": {
+				"anger": 0.0,
+				"contempt": 0.0,
+				"disgust": 0.0,
+				"fear": 0.0,
+				"happiness": 1.0,
+				"neutral": 0.0,
+				"sadness": 0.0,
+				"surprise": 0.0
+			}*/
+
+			//algorithm to determine the tempo base on emotions.
+			/*
+			return dataRecommendation = {
+
+				limit: 1,
+				seed_genres: genders, 
+				minTempo: 0.50,
+				maxTempo: 1.0
+			};
+		},*/
+		/*requestAPIAzure(imgFile) {
+
+			const SUB_KEY = '5a1b891888e1413ca3340a187c2f6f91';
+
+			return axios({
+
+				method: 'post',
+				url: 'https://development-faceapi.cognitiveservices.azure.com/face/v1.0/detect?overload=stream&returnFaceId=true&returnFaceAttributes=emotion&recognitionModel=recognition_01&detectionModel=detection_01',
+				data: data.imgFile,
+				headers: {
+
+					'Content-Type': 'application/octet-stream',
+					'Ocp-Apim-Subscription-Key': SUB_KEY
+				}
+			});
+			/*.then(response => console.log(response))
+			.catch(error => console.log(error));*/
+		/*},
+		requestAPISpotify(dataRecommendationParams) {
+
+			const AUTH_STR = 'Bearer '.concat('USER-TOKEN');
+
+			let config = {
+
+				headers: {
+
+					'Content-Type': 'application/json',
+					'Accept': 'application/json',
+					'Authorization': AUTH_STR
+				},
+				params: dataRecommendationParams
+			};
+
+			return axios.get('https://api.spotify.com/v1/recommendations', config);
+				/*.then(response => console.log(response))
+				.catch(error => console.log(error));*/
+		//}
 	}
 }
+
+/*
+[
+	{
+		"faceId": "4f688646-1515-4194-9b04-074f3b3eb3d0",
+		"faceRectangle": {
+			"top": 73,
+			"left": 343,
+			"width": 114,
+			"height": 114
+		},
+		"faceAttributes": {
+			"emotion": {
+				"anger": 0.0,
+				"contempt": 0.0,
+				"disgust": 0.0,
+				"fear": 0.0,
+				"happiness": 1.0,
+				"neutral": 0.0,
+				"sadness": 0.0,
+				"surprise": 0.0
+			}
+		}
+	}
+]
+*/
 
 </script>
 
